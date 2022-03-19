00e48c08012ee47f901c095c80161381368d3c6c
Kedar Kodgire <kedarkodgire.kk@gmail.com>
2022-03-19 23:15:58 +0530

    Change Image on Click

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+//        print("Button is Pressed")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+    }
+    
 }
 
